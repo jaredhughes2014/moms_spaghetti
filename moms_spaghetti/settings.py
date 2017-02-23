0ea4d4d1dd7a1eb5211716863a616ceb5317aacf
Jared Hughes <jared.t.hughes-1@ou.edu>
2017-02-23 14:58:41 -0600

    Deleted everything except for amazon's django tutorial

--- moms_spaghetti/settings.py
+++ moms_spaghetti/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_ekf0b!gs**gx8cn#j4s*b&053yny@2ogm=+37efdm#oh4+rgn'
+SECRET_KEY = 'xx@92=kpf2-5-dj+r7v$scb#4%40i4gs)p&n_3)90z4s4y5p-#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'moms_spaghetti.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,10 +74,10 @@ WSGI_APPLICATION = 'moms_spaghetti.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-      'ENGINE': 'django.db.backends.sqlite3',
-      'NAME': 'moms_spaghettti'
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
