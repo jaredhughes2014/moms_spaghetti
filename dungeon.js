289a115158329f80bc20a02036e983b1ad247272
Simon Boerwinkle <sboerwinkle@gmail.com>
2017-03-23 10:49:23 -0500

    Not pushing this yet

--- dungeon.js
+++ dungeon.js
@@ -1,7 +1,7 @@
 module.exports = handler;
 
 function handler(body) {
-    //body = JSON.parse(body)
+    //body = JSON.parse(body);
     return lambda_handler(body);
 }
 
@@ -57,7 +57,7 @@ my_tree = require('./dungeonTree.js');
 
 
 function get_slot(intent, slot_name) {
-    return intent.slots[slot_name].value
+    return intent.slots[slot_name].value;
 }
 
 
@@ -90,36 +90,37 @@ function moved_to(tree_index, attrs) {
     node = my_tree[tree_index];
     text = set_str_vars(node.text, attrs);
     retext = set_str_vars(node.retext, attrs);
-    if (node.opts) attrs.game_state = tree_index
-    else attrs = {}
+    if (node.opts) attrs.game_state = tree_index;
+    else attrs = {};
     //Possibly do "attrs.game_state = undefined" if we want the rest to persist?
-    return mk_reply('Game', text, retext, false, attrs)
+    return mk_reply('Game', text, retext, false, attrs);
 }
 
 function handle_conversation(intent, session) {
     body = get_slot(intent, 'HaveConversation').toLowerCase();
+    console.log(body);
     attrs = session.attributes || {};
     if (attrs.game_state !== undefined) {
-        place = attrs.game_state
+        place = attrs.game_state;
         node = my_tree[place];
         for (opt of node.opts) {
             if (opt.test(body, attrs)) {
-                return moved_to(opt.next_index, attrs)
+                return moved_to(opt.next_index, attrs);
             }
         }
         return mk_reply('Game', node.retext, node.retext, false, attrs);
     }
     if (body.includes('game')) return moved_to(0, attrs);
     for (phrase of exit_phrases) {
-        if (body.includes(phrase)) return mk_reply('Conversation', 'Goodbye!', null, true)
+        if (body.includes(phrase)) return mk_reply('Conversation', 'Goodbye!', null, true);
     }
     if (body.includes('help')) {
         return mk_reply('Conversation',
             'You can ask to leave, ask for this help, or ask to start the mini game',
             null, false
-        )
+        );
     }
-    return mk_reply('Conversation', body, null, false)
+    return mk_reply('Conversation', body, null, false);
 }
 
 
@@ -135,10 +136,10 @@ function on_launch(launch_request, session) {
     //Called when the user launches the skill without specifying what they want
     //print("on_launch requestId=" + launch_request.requestId + ", sessionId=" + session.sessionId)
 
-    body = "What would you like to talk about?"
-    reprompt = "I'm sorry, I didn't understand that"
+    body = "What would you like to talk about?";
+    reprompt = "I'm sorry, I didn't understand that";
 
-    return mk_reply("Welcome", body, reprompt, false)
+    return mk_reply("Welcome", body, reprompt, false);
 }
 
 
@@ -147,12 +148,12 @@ function on_intent(intent_request, session) {
 
     //print("on_intent requestId=" + intent_request.requestId + ", sessionId=" + session.sessionId)
 
-    intent = intent_request.intent
-    intent_name = intent_request.intent.name
+    intent = intent_request.intent;
+    intent_name = intent_request.intent.name;
 
     // Dispatch to your skill's intent handlers
     if (intent_name == "ConversationIntent") return handle_conversation(intent, session);
-    else throw "Invalid intent";
+    return mk_reply("Unknown Intent Name", "Couldn't handle intent name " + intent_name.replace(/([A-Z])/g, " $1"), null, true);
 }
 
 
@@ -162,6 +163,7 @@ function on_session_ended(session_ended_request, session) {
     Is not called when the skill returns should_end_session=true
     */
     console.log("on_session_ended requestId=" + session_ended_request.requestId + ", sessionId=" + session.sessionId);
+    return mk_reply("So long suckas", "So long suckas", null, true);
 }
 
 
@@ -194,5 +196,7 @@ function lambda_handler(evnt, context) {
         return on_intent(evnt.request, evnt.session);
     } else if (evnt.request.type == "SessionEndedRequest") {
         return on_session_ended(evnt.request, evnt.session);
+    } else {
+        return mk_reply("Unknown Request Type", "Couldn't handle request type " + evnt.request.type.replace(/[A-Z]/g, " $1"), null, true);
     }
 }
