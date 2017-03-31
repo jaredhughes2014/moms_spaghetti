03751746d62740b9c9a18352417907b958c27d11
jaredhughes2014 <j.hughes2014@ou.edu>
2017-03-31 11:05:51 -0500

    Partial progress

--- src/client/views/EditConversationView.js
+++ src/client/views/EditConversationView.js
@@ -1,16 +1,51 @@
 
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {browserHistory} from 'react-router';
+
+import paths from '../../paths';
+import ev from '../events';
+
+let events = ev.conversation;
 
 /**
  * View used to edit a conversation
  */
-const EditConversationView = () =>
+class EditConversationView extends React.Component
 {
-    return (
-        <div id="edit-conversation-root">
-            You're editing now
-        </div>
-    );
+    constructor(props)
+    {
+        super(props);
+    }
+
+    render()
+    {
+        return (
+            <div id="edit-root">
+                You're editing now
+            </div>
+        );
+    }
+}
+
+/**
+ * Maps the Redux state to the conversation window
+ */
+const mapStateToProps = (state) =>
+{
+    return {
+        conversation: state.conversation.conversations.find(p => p.name === state.conversation.active)
+    };
 };
 
-export default EditConversationView;
+/**
+ * Maps the redux event dispatcher to the conversation window
+ */
+const mapDispatchToProps = (dispatch) =>
+{
+    return {
+        goBack : () => dispatch(events.setActiveConversation.create(null))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditConversationView);
