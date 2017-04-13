bbf896097cb0c060578979a9edd7042e09894e89
jaredhughes2014 <j.hughes2014@ou.edu>
2017-04-13 11:34:20 -0500

    Landing page stuff

--- client/state/conversations.js
+++ client/state/conversations.js
@@ -57,6 +57,8 @@ const waitForConversations = {
 const reducer = (state=defaultState, event) =>
 {
     let args = event.args;
+    console.log(event);
+
     switch (event.type) {
 
         case (setConversations.type):
@@ -79,8 +81,8 @@ function* addConversationHandler(event)
     try {
         if (name) {
             yield put(wait());
-            const {conversation} = yield call(api.addConversation, name);
-            yield put({type: setConversations.type, args: {conversation}});
+            const {conversations} = yield call(api.addConversation, name);
+            yield put({type: setConversations.type, args: {conversations}});
         }
     }
     catch (err) {
