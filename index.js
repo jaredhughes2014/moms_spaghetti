d273628ec528c4f1678a8150bb0ffef0f6c60f2a
Brian Neldon <brian.neldon@gmail.com>
2017-04-13 10:36:23 -0500

    idk

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var dungeon = require('./src/server/dungeon.js');
+//var dungeon = require('./src/server/dungeon.js');
 
 var app = express();
 
@@ -21,15 +21,15 @@ app.get('/', (req, res) => {
 /**
  * Endpoint for alexa. This is where the user's text will be input for processing
  */
-app.post('/speak', (req, res) => {
-    if (req.body) {
-        console.log(req.body);
-        res.send(dungeon(req.body));
-    }
-    else {
-        res.send('There is an error with this skill');
-    }
-});
+// app.post('/speak', (req, res) => {
+//     if (req.body) {
+//         console.log(req.body);
+//         res.send(dungeon(req.body));
+//     }
+//     else {
+//         res.send('There is an error with this skill');
+//     }
+// });
 
 /**
  * Listen on port 8080 if a port isn't already defined by the environment
