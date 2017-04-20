2e5e673ba36b6d4a4903af67eabecd5fb55b8b9f
jaredhughes2014 <j.hughes2014@ou.edu>
2017-04-20 10:33:57 -0500

    Some stylez

--- client/views/AppSkeleton.js
+++ client/views/AppSkeleton.js
@@ -1,15 +1,27 @@
 
 
 import React from 'react';
+import {Link} from 'react-router';
+
+import routes from '../routes';
 
 /**
  * Top level view used to organize the structure of the app
  */
-const AppSkeleton = ({header, children}) =>
+const AppSkeleton = ({children}) =>
 {
     return (
         <div id="app-root">
-            <div id="app-header">{header}</div>
+            <div id="app-header">
+                <div id="app-title">
+                    Mom's Spaghetti
+                    <div id="app-subtitle">
+                        It's ready
+                    </div>
+                </div>
+                <Link to={routes.index}>Home</Link>
+            </div>
+
             <div id="app-content">{children}</div>
         </div>
     )
